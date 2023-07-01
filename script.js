b6e18de0f672b39ded660409f06442ceb61ab83a
YaKuBoVO <oyakubov526@gmail.com>
2023-07-01 18:11:29 +0500

    Prayekt tayyor

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
 "use strict"
+
+const itemList = document.querySelectorAll('.slider-item');
+const itemCount = itemList.length;
+
+
+
+
+const nextItem = document.querySelector('.next');
+const previousItem = document.querySelector('.previous');
+let count = 0;
+
+nextItem.addEventListener('click', showNext);
+previousItem.addEventListener('click', showPrevious);
+
+
+function showNext() {
+   itemList[count].classList.remove('active');
+   if(count < itemCount - 1) {
+      count++
+   } else {
+      count = 0;
+   }
+    itemList[count].classList.add('active');
+}
+
+function showPrevious() {
+   itemList[count].classList.remove('active');
+   if(count > 0) {
+      count--;
+   }  else {
+      count = itemCount -1;
+   } 
+   itemList[count].classList.add('active');
+}
